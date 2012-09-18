65bdb88cea0e210846c986db19a73fe56a1e15a4
Chima <Chima@Chima-PC>
2012-09-18 14:25:42 -0400

    Most of basic UI is included

--- UUVControlApp/gen/com/cmpen/uuvcontrolapp/R.java
+++ UUVControlApp/gen/com/cmpen/uuvcontrolapp/R.java
@@ -20,7 +20,28 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int AbsoluteLayout1=0x7f080000;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080006;
+        public static final int button3=0x7f080002;
+        public static final int button4=0x7f080005;
+        public static final int button5=0x7f08000a;
+        public static final int editText1=0x7f080008;
+        public static final int menu_settings=0x7f080015;
+        public static final int textView1=0x7f080001;
+        public static final int textView10=0x7f080011;
+        public static final int textView11=0x7f080012;
+        public static final int textView12=0x7f080013;
+        public static final int textView13=0x7f080014;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080009;
+        public static final int textView4=0x7f08000d;
+        public static final int textView5=0x7f08000c;
+        public static final int textView6=0x7f08000e;
+        public static final int textView7=0x7f08000b;
+        public static final int textView8=0x7f08000f;
+        public static final int textView9=0x7f080010;
+        public static final int toggleButton1=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
